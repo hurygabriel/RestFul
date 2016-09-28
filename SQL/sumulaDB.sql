2f9920a5df31a8de2d4067b6b3bc6f3601d9760a
BlueTub <hury_gabriel@hotmail.com>
2016-09-28 04:04:31 -0300

    Sql

--- SQL/sumulaDB.sql
+++ SQL/sumulaDB.sql
@@ -1,23 +1,22 @@
-use master
-go
-drop database sumula
+USE MASTER
+GO
+DROP DATABASE sumula
 ----------Query Estrutura das Tabelas--------------
-create database sumula
-go
-use sumula
+CREATE DATABASE sumula
+GO
+USE sumula
 
 
 CREATE TABLE clube(
 id int not null identity,
 datafundacao date not null,
-nome varchar(50),
-fundador varchar(100),
-
+nome VARCHAR(50),
+fundador VARCHAR(100),
 )
 
 CREATE TABLE titulo(
 id int not null identity,
-nome varchar(50),
+nome VARCHAR(50),
 PRIMARY KEY(id)
 )
 
@@ -25,33 +24,50 @@ CREATE TABLE clubetitulos(
 idtitulo int not null,
 idclube int not null,
 data date not null,
-foreign key (idclube) references clube(id),
-foreign key (idtitulo) references titulo(id),
+FOREIGN KEY (idclube) REFERENCES clube(id),
+FOREIGN KEY (idtitulo) REFERENCES titulo(id),
 )
 
 
 CREATE TABLE jogador(
 id int not null identity,
 datanasc date not null,
-posicao varchar(100) not null,
+posicao VARCHAR(100) not null,
 peso float,
 altura float,
-naturalidade varchar(50),
+naturalidade VARCHAR(50),
 PRIMARY KEY(id)
+)
+
+
+CREATE TABLE situacao(
+idSituacao int not null,
+idJogador int not null,
+situacao VARCHAR(20),
+primary key(idSituacao),
+FOREIGN KEY(idJogador) REFERENCES jogador(id)
+
+)
+
+CREATE TABLE escalacao(
+idEscalacao int not null,
+idJogador int not null,
+idSituacao int not null,
+
 )
 
 CREATE TABLE contrato(
 numero int not null,
-tipo varchar(30),
+tipo VARCHAR(30),
 datainicio date not null,
 datafim date,
 PRIMARY KEY(numero),
-foreign key (idclube) references clube(id),
-foreign key (idjogador) references jogador(id)
+FOREIGN KEY (idclube) REFERENCES clube(id),
+FOREIGN KEY (idjogador) REFERENCES jogador(id)
 )
 
 CREATE TABLE campeonato(
-nome varchar(50),
+nome VARCHAR(50),
 ano date
 )
 
